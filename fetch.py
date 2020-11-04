08b5d6bf4d70aa9d36bc43a3a4dc938e70e6d98c
Otto Lindblom <61689180+prOttonicFusion@users.noreply.github.com>
2020-11-04 15:07:34 +0200

    add configuration file

--- fetch.py
+++ fetch.py
@@ -1,11 +1,21 @@
 import argparse
+import configparser
 import urllib.request as request
 from htmlParser import CustomHTMLParser
 
-parser = argparse.ArgumentParser(
-    description='Fetch your GitHub contribution stats')
-parser.add_argument('-N', metavar='days', type=int, default=7, dest="dayDisplayCount",
-                    help='number of days to show (default: 7)')
+
+def initParse():
+    config = configparser.ConfigParser()
+    config.read("config.ini")
+
+    parser = argparse.ArgumentParser(
+        description='Fetch your GitHub contribution stats')
+    parser.add_argument('-N', metavar='days', type=int, default=7, dest="dayDisplayCount",
+                        help='number of days to show (default: 7)')
+    parser.add_argument('-u', '--user', metavar='userName', type=str, default=config['DEFAULT']['GitHubUsername'], dest="gitHubUser",
+                        help='specify GitHub username (default: read from config.ini)')
+
+    return parser.parse_args()
 
 
 def fetchProfilePage(gitHubUser):
@@ -29,15 +39,13 @@ def renderDatesForOutput(dateData, dayDisplayCount):
     return outPutString
 
 
-def main():
-    args = parser.parse_args()
-
-    gitHubUser = "prottonicfusion"
-    profilePage = fetchProfilePage(gitHubUser)
+def main(args):
+    profilePage = fetchProfilePage(args.gitHubUser)
     dateData = parseContributions(profilePage)
 
     print(renderDatesForOutput(dateData, args.dayDisplayCount))
 
 
 if __name__ == "__main__":
-    main()
+    args = initParse()
+    main(args)
